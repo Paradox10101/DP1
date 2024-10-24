e9e95a1050546678a12727feae5cb66ffe0dbb00
Paradox10101 <a20202467@pucp.edu.pe>
2024-10-24 01:13:53 -0500

    Cambios para MapLibre

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,6 +6,7 @@ import { DatePicker } from "@nextui-org/react"
 import PruebaSocket from "@/app/Components/PruebaSocket"
 import Simulacion from "@/app/Pages/Simulacion"
 
+//AQUI ES DESDE DONDE SE LLAMA LO PRINCIPAL
 export default function App(){
   return(
     <div className="relative">
