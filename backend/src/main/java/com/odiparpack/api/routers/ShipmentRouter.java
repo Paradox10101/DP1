8db03359d2f023efb564d5ae53c16bfbf1e1125a
darkoso7 <96436492+darkoso7@users.noreply.github.com>
2024-11-12 10:04:45 -0500

    Refactor ShipmentRouter to simplify order status handling and enhance vehicle position retrieval

--- backend/src/main/java/com/odiparpack/api/routers/ShipmentRouter.java
+++ backend/src/main/java/com/odiparpack/api/routers/ShipmentRouter.java
@@ -2,17 +2,16 @@ package com.odiparpack.api.routers;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.odiparpack.models.*;
+import com.odiparpack.models.Order;
+import com.odiparpack.models.Position;
+import com.odiparpack.models.SimulationState;
+import com.odiparpack.models.Vehicle;
 import spark.Spark;
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class ShipmentRouter extends BaseRouter {
     private final SimulationState simulationState;
@@ -26,28 +25,21 @@ public class ShipmentRouter extends BaseRouter {
         // Ruta para obtener posición de un vehículo específico
         Spark.get("/api/v1/shipments", (request, response) -> {
             List<Order>orders = simulationState.getOrders();
-            Map<String, Location>locations = simulationState.getLocations();
             JsonObject featureCollection = new JsonObject();
             featureCollection.addProperty("type", "FeatureCollection");
             JsonArray features = new JsonArray();
             JsonObject feature = new JsonObject();
             for (Order order : orders) {
-                String status = order.getStatus().toString();
                 feature.addProperty("orderCode", order.getId());
                 feature.addProperty("startTime", order.getOrderTime().format(DateTimeFormatter.ofPattern("dd/MM/yy HH:mm")));
-                if(!order.getStatus().equals(Order.OrderStatus.DELIVERED)){
-                    feature.addProperty("remainingTimeDays",  Duration.between(simulationState.getCurrentTime(), order.getDueTime()).toDays());
-                    feature.addProperty("remainingTimeHours", Duration.between(simulationState.getCurrentTime(), order.getDueTime()).toHours() % 24);
-                }
-                else{
-                        feature.addProperty("remainingTimeDays", "--");
-                        feature.addProperty("remainingTimeHours", "--");
-                }
-
-                feature.addProperty("originCity", locations.get(order.getOriginUbigeo()).getProvince());
-                feature.addProperty("destinyCity", locations.get(order.getDestinationUbigeo()).getProvince());
+                feature.addProperty("remainingTimeDays", Duration.between(order.getOrderTime(), order.getDueTime()).toDays());
+                feature.addProperty("remainingTimeHours", Duration.between(order.getOrderTime(), order.getDueTime()).toHours() % 24);
+                feature.addProperty("remainingTimeMinutes", Duration.between(order.getOrderTime(), order.getDueTime()).toMinutes() % 60);
+                feature.addProperty("originUbigeo", order.getOriginUbigeo());
+                feature.addProperty("destinyUbigeo", order.getDestinationUbigeo());
                 feature.addProperty("remainingPackages", order.getQuantity());
-                feature.addProperty("status", status);
+                feature.addProperty("status", order.getStatus().toString());
+
                 features.add(feature);
             }
             featureCollection.add("features", features);
@@ -55,59 +47,28 @@ public class ShipmentRouter extends BaseRouter {
             return gson.toJson(featureCollection);
         });
 
-
-
-        // Ruta para provocar averías
-        Spark.post("/api/v1/shipment", (request, response) -> {
+        /*
+        // Ruta para obtener todas las posiciones
+        Spark.get("/api/v1/vehicles/positions", (request, response) -> {
             response.type("application/json");
-            String orderIdSelected = request.queryParams("orderId");
-
-            Optional<Order> order = simulationState.getOrders().stream()
-                    .filter(ordenS -> ordenS.getId() == Integer.parseInt(orderIdSelected))
-                    .findFirst();
-
-            if(!order.isPresent()){
-                response.status(400);
-                return "{\"error\": \"Se debe ingresar un id de orden valido\"}";
-            }
-            Map<String, Location>locations = simulationState.getLocations();
-            JsonObject featureCollection = new JsonObject();
-            featureCollection.addProperty("type", "FeatureCollection");
-            JsonObject feature = new JsonObject();
-            String status = order.get().getStatus().toString();
-            JsonArray transportPlans = new JsonArray();
-
-            feature.addProperty("orderCode", order.get().getId());
-            feature.addProperty("startTime", order.get().getOrderTime().format(DateTimeFormatter.ofPattern("dd/MM/yy HH:mm")));
-            feature.addProperty("limitTime", order.get().getDueTime().format(DateTimeFormatter.ofPattern("dd/MM/yy HH:mm")));
-            if(!order.get().getStatus().equals(Order.OrderStatus.DELIVERED)&&!order.get().getStatus().equals(Order.OrderStatus.PENDING_PICKUP)){
-                feature.addProperty("elapsedTimeDays",  Duration.between(simulationState.getCurrentTime(), order.get().getDueTime()).toDays());
-                feature.addProperty("elapsedTimeHours", Duration.between(simulationState.getCurrentTime(), order.get().getDueTime()).toHours() % 24);
-            }
-            else{
-                feature.addProperty("elapsedTimeDays", "--");
-                feature.addProperty("elapsedTimeHours", "--");
-            }
-            feature.addProperty("originCity", locations.get(order.get().getOriginUbigeo()).getProvince());
-            feature.addProperty("destinyCity", locations.get(order.get().getDestinationUbigeo()).getProvince());
-            feature.addProperty("originUbigeo", locations.get(order.get().getOriginUbigeo()).getUbigeo());
-            feature.addProperty("destinyUbigeo", locations.get(order.get().getDestinationUbigeo()).getUbigeo());
-            feature.addProperty("totalPackages", order.get().getQuantity());
-            feature.addProperty("destinyRegion", locations.get(order.get().getDestinationUbigeo()).getNaturalRegion());
-            feature.addProperty("status", status);
-
-            for(VehicleAssignment assignment: simulationState.getAssignments()) {
-                JsonObject transportPlan = new JsonObject();
-                transportPlan.addProperty("type", "TransportPlan");
-                transportPlan.addProperty("vehicleCode",assignment.getVehicle().getCode());
-                transportPlan.addProperty("inTransportPackages",assignment.getAssignedQuantity());
-                transportPlans.add(transportPlan);
-            }
-            feature.add("transportPlans", transportPlans);
-            featureCollection.add("feature", feature);
-            return featureCollection.toString();
+            return getAllVehiclePositions();
         });
+        */
 
+        /*
+        // Ruta para provocar averías
+        Spark.post("/api/v1/vehicles/breakdown", (request, response) -> {
+            String vehicleCode = request.queryParams("vehicleCode");
+
+            if (vehicleCode == null || breakdownType == null) {
+                response.status(400);
+                return "{\"error\": \"vehicleCode y breakdownType son requeridos\"}";
+            }
+
+            simulationState.provocarAveria(vehicleCode, breakdownType);
+            return "{\"message\": \"Avería provocada exitosamente\"}";
+        });
+        */
     }
 }
 
