c493f14b971202c4be50bb936b48278520c56cf6
Ricardo Melendez <ricardomelendezao@gmail.com>
2024-12-17 23:54:04 -0500

    cambios op diarias

--- frontend/app/daily-operations/page.jsx
+++ frontend/app/daily-operations/page.jsx
@@ -12,14 +12,14 @@ import BlockedRoutesPanel from "../Components/BlockedRoutesPanel"
 import BreakdownPanel from "../Components/BreakdownPanel"
 import MaintenancePanel from "../Components/MaintenancePanel"
 import DailyOperationsModal from "../Components/daily-operations/DailyOperationsModal";
+import { useSimulationDiariaInit } from '../../hooks/useSimulationDiariaInit';
 
 const API_BASE_URL = process.env.NODE_ENV === 'production'
   ? process.env.NEXT_PUBLIC_API_BASE_URL_PROD
   : process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const DailyOperationsPage = () => {
-  const [simulationStatus, setSimulationStatus] = useAtom(simulationStatusAtom)
-  const [error, setError] = useAtom(simulationErrorAtom)
+  const { simulationStatus, simulationType, error } = useSimulationDiariaInit();
   const [performanceMetrics] = useAtom(performanceMetricsAtom)
   const [, setShowModal] = useAtom(showSimulationModalAtom)
   const [,setSimulationType] = useAtom(simulationTypeAtom);
@@ -31,25 +31,12 @@ const DailyOperationsPage = () => {
     topDestinations: []
   })
 
-  // Función para cargar métricas
-  const fetchOrdersMetrics = async () => {
-    try {
-      const response = await fetch(`${API_BASE_URL}/orders/daily-metrics`)
-      if (!response.ok) throw new Error('Error al cargar métricas')
-      const data = await response.json()
-      setOrdersMetrics(data)
-    } catch (error) {
-      console.error('Error cargando métricas:', error)
-      setError('Error al cargar métricas de órdenes')
-    }
-  }
-
   useEffect(() => {
     // Mostrar el modal y actualizar métricas cuando la simulación está detenida
     if (simulationStatus === 'stopped') {
       setShowModal(true)
-      fetchOrdersMetrics()
     }
+    
     setLoadingModal(true);
   }, [simulationStatus, setShowModal])
 
@@ -57,16 +44,14 @@ const DailyOperationsPage = () => {
     setSimulationType('diaria');
   }, [])
 
+  
+
   return (
     <>
-      { loadingModal &&
-        <DailyOperationsModal metrics={ordersMetrics} isOpenReport={isOpen} onCloseReport={onClose}/>
-      }
       <div className="relative w-screen h-screen">
         <PerformanceMetrics metrics={performanceMetrics} />
         <VehicleMap
           simulationStatus={simulationStatus}
-          setSimulationStatus={setSimulationStatus}
         />
         <SimulationPanel openReport={onOpen} />
         <MapLegend cornerPosition={"top-20 right-5"} />
